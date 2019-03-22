9431a5f52a3ab8a077118dc26e6085dbeb15d9ff
Кирилл <Kirill@MBP71.local>
2019-03-22 15:06:13 +0300

    send shit-UFO data

--- Server255.py
+++ Server255.py
@@ -1,5 +1,6 @@
 import socket
-# from scipy.signal import find_peaks_cwt
+from datetime import datetime
+
 
 # Set the socket parameters 
 # Адрес хоста (сервера): 
@@ -8,7 +9,6 @@ import socket
 # (в диапазоне 127.0.0.1 — 127.255.255.255), подключиться можно будет только с этого же компьютера.
 serverIP = '0.0.0.0'
 serverPort = 5000  # (от 0 до 65525, порты до 1024 зарезервированы для системы, порты TCP и UDP не пересекаются)
-bufferSize = 16  # Размер передаваемого сообщения, байт (1 символ = 1 байт)
 
 # Create a datagram socket
 # socket() - функция создания сокета
@@ -22,67 +22,158 @@ UDPServerSocket.bind((serverIP, serverPort))  # bind socket to host and port
 print("UDP server up and listening")
 # Listen for incoming datagrams
 # recvfrom - функция чтения UDP сообщений от клиента
-while True:
-    data, address = UDPServerSocket.recvfrom(bufferSize)
-    if data == b'ESP32':
-        print(f'Connected by ESP32 with IP Address: {address}')
-        break
-    elif data == b'Hello_UDP_Server':
+data = None
+while data != b'ESP32':
+    data, address = UDPServerSocket.recvfrom(16)  # '16' - размер передаваемого сообщения, байт (1 символ = 1 байт)
+    if data == b'Hello_UDP_Server':
         print(f"Message from Client: {data}")
         print(f"Client IP Address: {address}")
         UDPServerSocket.sendto(b'Hello_UDP_Client', address)  # sending a reply to client
+    # elif data == b'ESP32':
+    #     print(f'Connected by ESP32 with IP Address: {address}')
+    #     break
+print('Connected to the ESP32')
 
 answers = []
-while True:
-    data, address = UDPServerSocket.recvfrom(bufferSize)
-    if data == b'End':
-        break
+while data != b'End':
+    data, address = UDPServerSocket.recvfrom(10)
+    # if data == b'End':
+    #     break
     answers.append(data)
 print(f'Answers: {answers}')
 
 measurements = []
+time = []
 while True:
     data, address = UDPServerSocket.recvfrom(5)
-    temperature = (data[4] << 6 | data[3] >> 2) * 0.015625
-    measurements.append(temperature)
+    # temperature = (data[4] << 6 | data[3] >> 2) * 0.015625
+    measurements.append(data)
+    time.append(datetime.now().time())
 
 #%%
 UDPServerSocket.close()
-from matplotlib import pyplot as plt
-plt.plot(range(len(measurements)), measurements)
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import arange, abs as np_abs
+from numpy.fft import rfft, rfftfreq
+fd = 20  # sampling frequency
+
+#%% Open data from another file
+measurements = []
+with open("measurements_fd=20_len=697.txt") as file:
+    for i in file:
+        measurements.append(float(i))
+
+#%% Draw measurements
+plt.plot(arange(len(measurements))/float(fd), measurements)
+plt.title('Pulse wave')
+plt.xlabel('time, sec')
+plt.ylabel('temperature, t°')
+plt.grid()
 plt.show()
 
-#%%
-fd = 20  # частота дискретизации, кол-во отсчётов в секунду (в дискретном сигнале представлены частоты от 0.75 до 4 Гц)
-# plt.plot(arange(len(measurements))/float(fd), measurements)  # исходный сигнал
-# plt.title('Исходный сигнал')
-# plt.xlabel('Время, с')
-# plt.ylabel('Температура, t°')
-# plt.grid(True)
-# plt.show()
+#%% Draw measurements with peaks with use continuous wavelet transform
+from scipy.signal import find_peaks_cwt
+findPeaks = find_peaks_cwt(np.array(measurements), range(1, len(measurements)), min_length=1)
+peaks = [measurements[i] for i in findPeaks]
+plt.plot(range(len(measurements)), measurements)
+plt.scatter(findPeaks, peaks, color='red')
 
-from numpy import abs as np_abs
-from numpy.fft import rfft, rfftfreq
+#%% Draw measurements with peaks
+from scipy.signal import find_peaks
 
+x = np.array(measurements)
+peaks1, _ = find_peaks(x, distance=20)
+peaks2, _ = find_peaks(x, prominence=0.01)
+peaks3, _ = find_peaks(x, width=10)
+peaks4, _ = find_peaks(x, threshold=0.01)
+plt.subplot(2, 2, 1)
+plt.plot(peaks1, x[peaks1], "xr"); plt.plot(x); plt.legend(['distance'])
+plt.subplot(2, 2, 2)
+plt.plot(peaks2, x[peaks2], "ob"); plt.plot(x); plt.legend(['prominence'])
+plt.subplot(2, 2, 3)
+plt.plot(peaks3, x[peaks3], "vg"); plt.plot(x); plt.legend(['width'])
+plt.subplot(2, 2, 4)
+plt.plot(peaks4, x[peaks4], "xk"); plt.plot(x); plt.legend(['threshold'])
+plt.show()
+
+#%% Draw spectrum
 spectrum = rfft(measurements)  # прямое одномерное ДПФ (для действительных сигналов)
 
-plt.plot(rfftfreq(len(measurements), 1./(len(measurements)/fd)), np_abs(spectrum)/len(measurements))
-plt.title('Спектр исходного сигнала')
-# plt.ylim([0, 0.15])
-# plt.xlim([0, 20])
+plt.plot(60*rfftfreq(len(measurements), 1./(len(measurements)/fd)), np_abs(spectrum)/len(measurements))
+plt.title('Pulse wave spectrum')
+plt.ylim([0, 0.005])
+# plt.xlim([0, 200])
+plt.grid()
 plt.show()
 
+#%% Draw spectrum with peaks
+spectrum = rfft(measurements)  # прямое одномерное ДПФ (для действительных сигналов)
+freq = rfftfreq(len(measurements), 1./(len(measurements)/fd))
+ampl = np_abs(spectrum)/len(measurements)
+
+peaks5, _ = find_peaks(ampl, height=0.001)
+plt.figure()
+plt.plot(peaks5/fd, ampl[peaks5], "xr")
+plt.plot(freq, ampl)
+plt.xlabel('frequency, Hz')
+plt.ylabel('magnitude')
+plt.ylim([0, 0.05])
+plt.xlim([0, 10])
+plt.grid()
+plt.show()
+
+#%% Calculate heart rate
+val, i = max((val, i) for i, val in enumerate(ampl[peaks5]))
+HR = 60*peaks5[i]/fd
+plt.plot(peaks5[i]/fd, ampl[peaks5][i], "xg")
+plt.title(f'Pulse wave spectrum\nHR = {HR} bps')
+# plt.text(8.5, .0044, f'HR: {HR}', fontsize=20)
+
+#%% Save measurements
+with open(f"measurements_fd={fd}_len={len(measurements)}.txt", 'w') as file:
+    for i in measurements:
+        file.write(str(i) + '\n')
+print('end')
+
+#%% Save data time
+with open(f"data_time_len={len(time)}.txt", 'w') as file:
+    for i in time:
+        file.write(str(i) + '\n')
+print('end')
+
+#%% Draw plot with animation
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib.animation import FuncAnimation
+j = 0
 
 #%%
-# if len(measurements) == n:
-#     plt.plot(range(len(measurements)), measurements)
-#     plt.xlim((n - fd) // fd, n // fd)
-#     plt.show()
-#     n = n + fd
-#     if len(measurements) == 800:
-#         break
+style.use('seaborn')
+
+fig, ax = plt.subplots()
 
 
+def animate(i):
+    ax.clear()
+    ax.set_xlim(j, 10+j)
+    ax.plot(arange(len(measurements))/float(fd), measurements)
+
+
+anim = FuncAnimation(fig, animate, interval=2000)
+plt.show()
+
+#%%
+average = sum(measurements)/len(measurements)
+measurementsMinusAvg = []
+for i in measurements:
+    measurementsMinusAvg.append(i - average)
+measurementsMinusAvgFFT = rfft(measurementsMinusAvg)
+
+plt.plot(rfftfreq(len(measurementsMinusAvgFFT), 1./(len(measurements)/fd)), measurementsMinusAvgFFT)
+
+#%%
 # measurements = []
 # noneData = []
 # fd = 20  # частота дискретизации
@@ -109,13 +200,8 @@ plt.show()
 #             plt.show()
 #             n = n + fd
 #             if len(measurements) == 800:
-#                 fig.savefig('measurements_fd=20_len=800.pdf')
+#                 fig.savefig(f'measurements_fd={fd}_len={len(measurements)}.pdf')
 #                 break
 #                 # raw_input = input('Would you like to continue?\n Press "y" if yes or "n" if not')
 #                 # if raw_input == 'n':
 #                 #     break
-#
-# with open("measurements_fd=20_len=800.txt", 'w') as file:
-#     for i in measurements:
-#         file.write(str(i) + '\n')
-# print('end')
--- main.py
+++ main.py
@@ -7,31 +7,48 @@ import uos
 import time
 
 
-class Interrupt(object):
-    def __enter__(self):
-        self._startTime = time.time()
-
-    def __exit__(self, type, value, traceback):
-        elapsed_time = "Elapsed time: {:.3f} sec".format(time.time() - self._startTime)
-        UDPClientSocket.sendto(elapsed_time.encode(), (UDP_IP, serverPort))
-
-
-def read_and_send(comport, request, lst, len_lst):
-    comport.write(request)
-    time.sleep(0.025)
-    data = comport.read(5)  # '5' - кол-во принимаемых платой байт
-    lst.append(data)
-    if len(lst) == len_lst:
-        for i, val in enumerate(lst):
-            UDPClientSocket.sendto(val, (UDP_IP, serverPort))
-        lst.clear()
+# class Interrupt(object):
+#     def __enter__(self):
+#         self._startTime = time.time()
+#
+#     def __exit__(self, type, value, traceback):
+#         elapsed_time = "Elapsed time: {:.3f} sec".format(time.time() - self._startTime)
+#         UDPClientSocket.sendto(elapsed_time.encode(), (UDP_IP, serverPort))
 
 
-def send_bytes(comport, request, sock, ip, port):
-    comport.write(request)
-    time.sleep(1)
-    answer = comport.read()
-    sock.sendto(answer, (ip, port))
+# def read_and_send(comport, request, n_bytes, lst, len_lst):
+#     comport.write(request)
+#     data = comport.read(n_bytes)
+#     lst.append(data)
+#     if len(lst) == len_lst:
+#         for val in lst:
+#             UDPClientSocket.sendto(val, (UDP_IP, serverPort))
+#         lst.clear()
+
+
+# def read_and_send2(request, n_bytes):
+#     uart.write(request)
+#     UDPClientSocket.settimeout(0.05)
+#     UDPClientSocket.sendto(_, (UDP_IP, serverPort))
+#     uart.read(n_bytes)
+
+
+# def read_and_send(comport, request, lst, len_lst):
+#     comport.write(request)
+#     time.sleep(0.025)
+#     data = comport.read(5)  # '5' - кол-во принимаемых платой байт
+#     lst.append(data)
+#     if len(lst) == len_lst:
+#         for i, val in enumerate(lst):
+#             UDPClientSocket.sendto(val, (UDP_IP, serverPort))
+#         lst.clear()
+
+
+# def send_bytes(comport, request, sock, ip, port):
+#     comport.write(request)
+#     time.sleep(1)
+#     answer = comport.read()
+#     sock.sendto(answer, (ip, port))
 
 
 sta_if = network.WLAN(network.STA_IF)
@@ -53,27 +70,62 @@ msg, addr = UDPClientSocket.recvfrom(16)
 if msg == b'Hello_UDP_Client':
     UDPClientSocket.sendto(b'ESP32', (UDP_IP, serverPort))
 
+# from socket import setdefaulttimeout
+# UDPClientSocket.setdefaulttimeout(10.0)
+
 esp.osdebug(None)
 uos.dupterm(None, 0)
-# timeout=0
+
 uart = UART(2, 115200)  # создание объекта UART, "2"- Номер UART-порта микроконтроллера
-uart.init(115200, bits=8, parity=None, stop=1)  # initialization the UART
+uart.init(115200, bits=8, parity=None, stop=1, timeout=25)  # initialization the UART
 
 micropython.kbd_intr(-1)  # запрет умирания платы в случае, когда датчик возвращает байт, который равен "C"
-micropython.alloc_emergency_exception_buf(100)  # for interrupt
+# micropython.alloc_emergency_exception_buf(100)  # for interrupt
 uart.read()  # чтение данных из входного буфера микроконтроллера и его очистка
 
-with Interrupt():
-    send_bytes(uart, b'\x55\x95\x0D', UDPClientSocket, UDP_IP, serverPort)  # initialization the sensor
-    send_bytes(uart, b'\x55\x08\xA0\x01\x00', UDPClientSocket, UDP_IP, serverPort)  # EEPROM unlock
-    send_bytes(uart, b'\x55\x0A\xA0', UDPClientSocket, UDP_IP, serverPort)  # check EEPROM unlock state
-    send_bytes(uart, b'\x55\x08\xA1\x00\x20', UDPClientSocket, UDP_IP, serverPort)  # изменение регистра
-    # конфигурации (принимаем fd=20)
-    send_bytes(uart, b'\x55\x08\xA0\x00\x00', UDPClientSocket, UDP_IP, serverPort)  # EEPROM lock
-    send_bytes(uart, b'\x55\x0A\xA1', UDPClientSocket, UDP_IP, serverPort)  # check EEPROM conversion rate
-    send_bytes(uart, b'\x55\x0A\xA0', UDPClientSocket, UDP_IP, serverPort)  # check EEPROM lock state
-UDPClientSocket.sendto(b'End', (UDP_IP, serverPort))  # completing the configuration
+# with Interrupt():
+#     send_bytes(uart, b'\x55\x95\x0D', UDPClientSocket, UDP_IP, serverPort)  # initialization the sensor
+#     send_bytes(uart, b'\x55\x08\xA0\x01\x00', UDPClientSocket, UDP_IP, serverPort)  # EEPROM unlock
+#     send_bytes(uart, b'\x55\x0A\xA0', UDPClientSocket, UDP_IP, serverPort)  # check EEPROM unlock state
+#     send_bytes(uart, b'\x55\x08\xA1\x00\x20', UDPClientSocket, UDP_IP, serverPort)  # изменение регистра
+#     # конфигурации (принимаем fd=20)
+#     send_bytes(uart, b'\x55\x08\xA0\x00\x00', UDPClientSocket, UDP_IP, serverPort)  # EEPROM lock
+#     send_bytes(uart, b'\x55\x0A\xA1', UDPClientSocket, UDP_IP, serverPort)  # check EEPROM conversion rate
+#     send_bytes(uart, b'\x55\x0A\xA0', UDPClientSocket, UDP_IP, serverPort)  # check EEPROM lock state
+# UDPClientSocket.sendto(b'End', (UDP_IP, serverPort))  # completing the configuration
 
-buffer = []
+
+# buffer = []
+requests = (b'\x55\x95\x0D', b'\x55\x08\xA0\x01\x00', b'\x55\x0A\xA0', b'\x55\x08\xA1\x00\x20',
+            b'\x55\x08\xA0\x00\x00', b'\x55\x0A\xA1', b'\x55\x0A\xA0')
+# for value in requests:
+#     read_and_send(uart, value, 10, buffer, 7)
+# UDPClientSocket.sendto(b'End', (UDP_IP, serverPort))  # completing the configuration
+
+# while True:
+#     read_and_send(uart, b'\x55\x0A\xA0', 5, buffer, 1000)
+
+data = b'Start'
+for value in requests:
+    uart.write(value)
+    UDPClientSocket.settimeout(1.0)
+    UDPClientSocket.sendto(data, (UDP_IP, serverPort))
+    data = uart.read()
+
+UDPClientSocket.sendto(data, (UDP_IP, serverPort))
+data = b'End'
 while True:
-    read_and_send(uart, b'\x55\x0A\xA0', buffer, 50)
+    uart.write(b'\x55\x0A\xA0')
+    UDPClientSocket.settimeout(1.0)
+    time.sleep(1)
+    UDPClientSocket.sendto(data, (UDP_IP, serverPort))
+    data = uart.read(5)
+
+# not worked
+# b'Start'
+# for value in requests:
+#     read_and_send2(value, 10)
+# b'End'
+#
+# while True:
+#     read_and_send2(b'\x55\x0A\xA0', 5)
